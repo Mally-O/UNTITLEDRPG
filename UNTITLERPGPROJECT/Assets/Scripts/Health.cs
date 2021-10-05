86dc9db576e07754104ecec56da10283385045b1
MicMetz <mime9599@colorado.edu>
2021-10-04 23:28:33 -0600

    Health class draft implementation.

--- UNTITLERPGPROJECT/Assets/Scripts/Health.cs
+++ UNTITLERPGPROJECT/Assets/Scripts/Health.cs
@@ -2,11 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+    /*
+     *
+     */
 public class Health : MonoBehaviour
 {
+    // Entity health property.
     [SerializeField] private int health_ = 100;
 
 
+        /*
+         * <summary>
+         *  Method to maintain watch over the entity's living state
+         *  in relation to the object's health status.
+         * </summary>
+         * <param name=""></param>
+         * <returns>void</returns>
+         */
     private void _Update_Status()
     {
         if (health_ <= 0)
@@ -16,20 +30,44 @@ public class Health : MonoBehaviour
     }
 
 
+        /*
+         * <summary>
+         *  Method to initiate death sequence in response to
+         *  entity's health falling to or below zero.
+         * </summary>
+         * <param name=""></param>
+         * <returns>void</returns>
+         */
     private void _Death()
     {
         Destroy(gameObject);
     }
 
 
+        /*
+         * <summary>
+         *  Method for getting entity's health.
+         * </summary>
+         * <param name=""></param>
+         * <returns>returns int value representation of health.</returns>
+         */
     public int _GetHealth() const
     {
         return health_;
     }
 
 
+        /*
+         * <summary>
+         *  Method to update the health of the entity.
+         * </summary>
+         * <param name=""></param>
+         * <returns>void</returns>
+         */
     public void _SetHealth(int adj_value)
     {
         health_ += adj_value;
     }
+
+
 }
